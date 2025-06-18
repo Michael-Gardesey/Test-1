66f429cf14b4e277148925c3145ccd258d776c95
Michael-Gardesey <95989340+Michael-Gardesey@users.noreply.github.com>
2025-06-18 07:16:27 +0000

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-8 text-center bg-gray-100 dark:bg-gray-900 mt-12">
+      <span>&copy; {new Date().getFullYear()} Michael Gardesey. All rights reserved.</span>
+    </footer>
+  );
+}
