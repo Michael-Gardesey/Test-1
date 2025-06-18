c2a3486799fe82ca60e2da5add261fb2f76808bf
Michael-Gardesey <95989340+Michael-Gardesey@users.noreply.github.com>
2025-06-18 09:24:05 +0000

    Create getContent.js

---
+++ utils/getContent.js
@@ -0,0 +1,19 @@
+// /utils/getContent.js
+
+// Placeholder: use real file reading (fs) and parsing when you add content files
+
+export function getProjects() {
+  // When ready, use fs and path to read from /content/projects/
+  // e.g., return JSON.parse(fs.readFileSync(path.join(process.cwd(), 'content/projects/projects.json')));
+  return [];
+}
+
+export function getSpeaking() {
+  // When ready, use fs and path to read from /content/speaking/
+  return [];
+}
+
+export function getTestimonials() {
+  // When ready, use fs and path to read from /content/testimonials/
+  return [];
+}
